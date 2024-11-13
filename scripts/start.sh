fc32e661c70a7fc23b0cba12cd923f5d42ddcaf7
Avneep Dhanju <avdhanju@microsoft.com>
2024-11-13 00:05:33 +0000

    Fixes version and author

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,6 @@
- echo "Starting Grafana"
-  mkdir -p $WORKSPACE_FOLDER/grafana/plugins/azure-monitor-app
-  ln -s ./dist ./grafana/plugins/azure-monitor-app
-  cd /home/vscode/grafana/bin 
-  ./grafana server -config $WORKSPACE_FOLDER/grafana/grafana.ini 
+echo "Starting Grafana"
+rm -rf $WORKSPACE_FOLDER/grafana/plugins/azure-monitor-app
+mkdir -p $WORKSPACE_FOLDER/grafana/plugins/azure-monitor-app
+ln -s $WORKSPACE_FOLDER/dist $WORKSPACE_FOLDER/grafana/plugins/azure-monitor-app
+cd /home/vscode/grafana/bin 
+./grafana server -config $WORKSPACE_FOLDER/grafana/grafana.ini 
