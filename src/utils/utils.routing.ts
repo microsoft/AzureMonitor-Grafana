62a0f9140421e5c52dd968cad951abcebf12ef19
Jocelyn <jcolladokuri@microsoft.com>
2025-01-14 22:11:53 +0000

    fix trailing forward slash

--- src/utils/utils.routing.ts
+++ src/utils/utils.routing.ts
@@ -2,5 +2,5 @@ import { PLUGIN_BASE_URL, ROUTES } from '../constants';
 
 // Prefixes the route with the base URL of the plugin AND home route clusternavigation
 export function prefixRoute(route: string): string {
-  return `${PLUGIN_BASE_URL}/${ROUTES.Home}/${route}`;
+  return `${PLUGIN_BASE_URL}/${ROUTES.Home}${!!route ? `/${route}` : ""}`;
 }
