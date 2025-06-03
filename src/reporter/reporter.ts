323783eab8a5c1b7c44f2701ebf1518a518b72fd
jcolladokuri <jocelyncollado52@gmail.com>
2025-06-03 11:01:12 -0700

    update grafana packages to mitigate uplot vulnerability (#35)

--- src/reporter/reporter.ts
+++ src/reporter/reporter.ts
@@ -2,23 +2,23 @@ import { PluginContextType } from "@grafana/data";
 import { reportInteraction } from "@grafana/runtime";
 
 export class Reporter {
-    private pluginContext: PluginContextType;
-    constructor(pluginContext: PluginContextType) {
+    private pluginContext: PluginContextType | null;
+    constructor(pluginContext: PluginContextType | null) {
         this.pluginContext = pluginContext;
     }
 
     public reportException(exceptionName: string, properties: Record<string, unknown>) {
-        const newProperties = {...this.pluginContext.meta, ...properties};
+        const newProperties = {...this.pluginContext?.meta, ...properties};
         reportInteraction(exceptionName, newProperties);
     }
 
     public reportPageView(pageName: string, properties: Record<string, unknown>) {
-        const newProperties = {...this.pluginContext.meta, ...properties};
+        const newProperties = {...this.pluginContext?.meta, ...properties};
         reportInteraction(pageName, newProperties);
     }
     
     public reportEvent(eventName: string, properties: Record<string, unknown>) {
-        const newProperties = {...this.pluginContext.meta, ...properties};
+        const newProperties = {...this.pluginContext?.meta, ...properties};
         reportInteraction(eventName, newProperties);
     }
 }
